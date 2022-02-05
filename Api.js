a1e3507c2361d917e1a06299191634be03782f42
Naqash-Muhammad <92191163+Naqash-Muhammad@users.noreply.github.com>
2022-02-05 02:03:55 -0700

    KeepApi

---
+++ Api.js
@@ -0,0 +1,49 @@
+[
+    {
+        id : 0,
+        Name : "Naqash",
+        Age : 22,
+        Edu : "BsCs",
+        Adress : {
+            City : "Lahore",
+            Street : "Rehman Pura",
+            Houes : "12 Num"
+        }
+    },
+
+    {
+        id : 0,
+        Name : "Naqash",
+        Age : 22,
+        Edu : "BsCs",
+        Adress : {
+            City : "Lahore",
+            Street : "Rehman Pura",
+            Houes : "12 Num"
+        }
+    },
+
+    {
+        id : 0,
+        Name : "Naqash",
+        Age : 22,
+        Edu : "BsCs",
+        Adress : {
+            City : "Lahore",
+            Street : "Rehman Pura",
+            Houes : "12 Num"
+        }
+    },
+
+    {
+        id : 0,
+        Name : "Naqash",
+        Age : 22,
+        Edu : "BsCs",
+        Adress : {
+            City : "Lahore",
+            Street : "Rehman Pura",
+            Houes : "12 Num"
+        }
+    },
+]
